b16007c19837a15e5e98e62d1316b4ec3b1a9090
Pedro Chacón <pedrochacon9621@gmail.com>
2025-08-29 15:08:43 +0200

    agregando enlace politica

--- Frontend-AppCorreos/src/App.tsx
+++ Frontend-AppCorreos/src/App.tsx
@@ -6,7 +6,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 function App() {
   
 useEffect(() => {
-    alert("Esta aplicación es una versión de prueba con fines demostrativos. No realiza almacenamiento ni procesamiento de datos personales. Toda la información ingresada —como correos, archivos o mensajes— se gestiona únicamente en el navegador del usuario y no se transmite a servidores externos. No está diseñada para uso comercial ni para el envío real de correos electrónicos. Se recomienda no ingresar información sensible durante su uso. Para consultar la política completa, haz clic ");
+    alert("Esta aplicación es una versión de prueba con fines demostrativos. No realiza almacenamiento ni procesamiento de datos personales. Toda la información ingresada —como correos, archivos o mensajes— se gestiona únicamente en el navegador del usuario y no se transmite a servidores externos. No está diseñada para uso comercial ni para el envío real de correos electrónicos. Se recomienda no ingresar información sensible durante su uso. Puede consulta la política de privacidad completa en el enlace");
   }, []);
   
 
@@ -14,6 +14,7 @@ useEffect(() => {
     <>
       <Router>
         <Routes>
+          <link rel="stylesheet" href="/politica" />
           <Route path="/" element={<BusquedaArchivo/>}/>
           <Route path="/politica" element={<PoliticaPage/>}/>
         </Routes>
