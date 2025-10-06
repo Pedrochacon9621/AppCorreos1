52c5f3a3a3779b26db7c43df4d35038b8930475a
Pedro Chacón <pedrochacon9621@gmail.com>
2025-10-06 20:51:37 +0200

    modificando para enviar correos con api 3

--- Frontend-AppCorreos/src/js/fetch.ts
+++ Frontend-AppCorreos/src/js/fetch.ts
@@ -1,6 +1,6 @@
 /*const api = "http://localhost:3000"*/
-const api = "https://backend-appcorreos1.onrender.com" //produccion
-
+//const api = "https://backend-appcorreos1.onrender.com" //produccion
+const api="https://appcorreos1-production.up.railway.app"
 export const enviarFileBackend = async (files:FormData)=>{
     
     
